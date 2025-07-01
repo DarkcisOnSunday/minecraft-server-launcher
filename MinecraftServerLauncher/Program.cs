49a386e6fb0c43ae76ae6c882412a06b63eec21c
Darkcis <maximka289805@gmail.com>
2025-07-01 12:30:35 +0300

    Init app

---
+++ MinecraftServerLauncher/Program.cs
@@ -0,0 +1,11 @@
+namespace MinecraftServerLauncher;
+
+static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
